a1a35fbc36e95be02233a7e76026db6066280969
Kuznetsov Denis <kuznetsov.den.p@gmail.com>
2018-02-04 18:11:36 +0300

    test (logging): Add output in flask_app

--- flask_app.py
+++ flask_app.py
@@ -3,21 +3,29 @@
 
 from flask import Flask, request, json
 from settings import *
-import messageHandler
+# import messageHandler
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello from Flask!'
+    response = 'Hello from Flask!'
+    print(response)
+    return response
 
 @app.route('/', methods=['POST'])
 def processing():
     data = json.loads(request.data)
     if 'type' not in data.keys():
-        return 'not vk'
+        response = 'not vk'
+        print(response)
+        return response
     if data['type'] == 'confirmation':
-        return confirmation_token
+        response = confirmation_token
+        print(response)
+        return response
     elif data['type'] == 'message_new':
-        messageHandler.create_answer(data['object'], token)
-        return 'ok'
+        # messageHandler.create_answer(data['object'], token)
+        response = 'ok'
+        print(response)
+        return response
